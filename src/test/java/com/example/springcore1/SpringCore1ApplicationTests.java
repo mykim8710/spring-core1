9d83afee529c3a1584b8ccc5f887b48aa28ccf8a
mykim <mykim8710@gmail.com>
2022-08-08 21:22:16 +0900

    [ add ] Initial commit

---
+++ src/test/java/com/example/springcore1/SpringCore1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springcore1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCore1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
