8c4fbf6d379172bd25bca74b6498a9710f78ad77
mykim <mykim8710@gmail.com>
2022-08-14 19:46:28 +0900

    [ dev ] XML기반의 스프링 설정 TestCode

---
+++ src/test/java/com/example/springcore1/xmlconfig/XmlAppConfigTest.java
@@ -0,0 +1,19 @@
+package com.example.springcore1.xmlconfig;
+
+import com.example.springcore1.member.MemberService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class XmlAppConfigTest {
+    @Test
+    @DisplayName("XML기반의 스프링설정 테스트 - appConfig.xml")
+    void xmlAppContext() {
+        ApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberService.class);
+    }
+}
